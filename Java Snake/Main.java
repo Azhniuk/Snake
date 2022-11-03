c574b82e49b2b75d6f56e705b854c33ac419e4b0
Sofia <azhniuk.s@gmail.com>
2022-11-03 23:48:05 +0300

    Вкрала чийсь код(((((

--- Java Snake/Main.java
+++ Java Snake/Main.java
@@ -1,35 +1,6 @@
-class Main {
-    //To check if the game is ended
-    boolean gameOver = false;
-
-    //size of a square
-    int s = 4;
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-
-    public void endGame(){
-        gameOver = true;
-        background(0);
-
-        fill(255);
-
-        text("Gameover", 10, 20); 
-      }
-      
-
-      public void reset() {
-        gameOver = false;
-        
-
-        snakePositions.clear();
-        
-        PVector c = new PVector(width/2, height/2);
-        
+public class Main {
+  public static void main(String[] args) throws Exception {
+    new Frame();
     
-        snakePositions.add(c);
-        
-        randomize(objPosition, 0, width-s); 
-      }
+  }
 }
