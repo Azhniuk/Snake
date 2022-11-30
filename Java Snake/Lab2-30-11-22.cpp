5d4a1a4c8dbda2fa178413e0103f819227e47232
Sofia <azhniuk.s@gmail.com>
2022-11-30 16:14:42 +0300

    Update Lab2-30-11-22.cpp

--- Java Snake/Lab2-30-11-22.cpp
+++ Java Snake/Lab2-30-11-22.cpp
@@ -2,7 +2,9 @@
 #include <stack>
 #include <vector>
 
-using std::cout; using std::endl;
+using std::cout; 
+
+
 int contains(char to_check, std::string to_check_list) {
   for (unsigned int i = 0; i < to_check_list.size(); i++) {
     if (to_check_list[i] == to_check) {
@@ -12,7 +14,8 @@ int contains(char to_check, std::string to_check_list) {
   return -1;
 }
 
-bool bracesCheck(std::string all_braces) {
+
+bool Check(std::string all_braces) {
   std::string open_braces = "[({";
   std::string close_braces = "])}";
 
@@ -22,12 +25,13 @@ bool bracesCheck(std::string all_braces) {
     int value = contains(all_braces[i], open_braces);
     if (value != -1) {
       element_stack.push(all_braces[i]);
-      std::cout << "Pushed: " << all_braces[i] << std::endl;
-    } else {
+    } 
+    else {
       unsigned int end_index = contains(all_braces[i], close_braces);
       char open_braces_element = open_braces[end_index];
-      std::cout << "Popped: " << element_stack.top() << " for across " << all_braces[i] << std::endl;
-      if (element_stack.top() == open_braces_element) {
+    
+      if (element_stack.top() == open_braces_element) 
+      {
         element_stack.pop();
         continue;
       } else {
@@ -48,10 +52,14 @@ std::string Convert(bool result){
     }
 }
 
+
 int main() {
-  std::string list_of_braces = "({[()]})";
-  bool result = bracesCheck(list_of_braces);
+    
+    std::string str;
+    std::cout <<"Your braces: ";
+    std:: cin >> str;
+    bool result = Check(str);
   
-  std::cout << "Result: " << Convert(result) ;
+    Convert(result) ;
   return 0;
 }
